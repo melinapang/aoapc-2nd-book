6ddc9bd6ecea63786a9bc9475e023a7ca1471f81
lazydooog <444836458@qq.com>
2020-03-19 00:05:27 +0800

    update chapter 3

--- chapter_3/UVa_232.cpp
+++ chapter_3/UVa_232.cpp
@@ -2,15 +2,18 @@
  * @Descripttion:习题3-6 纵横字谜的答案（Crossword Answers，ACM/ICPC WorldFinals 1994，UVa232） 
  * @Author: Xiaobin Ren
  * @Date: 2020-03-18 20:39:27
- * @LastEditTime: 2020-03-18 21:11:57
+ * @LastEditTime: 2020-03-18 23:38:48
  */
 
 #include <bits/stdc++.h>
+
+typedef struct {double x; double y;} point;
 
 using namespace std;
 
 int main(){
 
-    
+    point a;
+    a.x = 2.0;    
     return 0;
 }
